a365aaed4a9597d0deecc9f0aa2c99ea7b7f0cab
Mohamed Ashraf <ebtikardev@gmail.com>
2025-11-22 19:47:21 +0200

    removed page sections

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -12,12 +12,12 @@ export default function IndexPage() {
     <>
       <HeroLanding />
       <PreviewLanding />
-      <Powered />
-      <BentoGrid />
-      <InfoLanding data={infos[0]} reverse={true} />
+      {/*<Powered />*/}
+      {/*<BentoGrid />*/}
+      {/*<InfoLanding data={infos[0]} reverse={true} />*/}
       {/* <InfoLanding data={infos[1]} /> */}
-      <Features />
-      <Testimonials />
+      {/*<Features />*/}
+      {/*<Testimonials />*/}
     </>
   );
 }
